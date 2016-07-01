3e7537c175085a4efdd56964feb98b2dde7ef941
崔海 <cuihai_lte@163.com>
2016-07-01 20:48:47 +0800

    第3次测试

--- app/src/main/java/com/example/mygittest/MainActivity.java
+++ app/src/main/java/com/example/mygittest/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.mygittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextClock;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
  private TextView tv;
+    private TextClock textClock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
